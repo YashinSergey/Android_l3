152beac300633afe5f370e2cab7d185937d20a64
mr.Blue <yashinsergey125@gmail.com>
2019-09-14 18:46:59 +0300

    Removed switch from presenter

--- MVP/src/main/java/com/example/mvp/Presenter.java
+++ MVP/src/main/java/com/example/mvp/Presenter.java
@@ -10,29 +10,13 @@ class Presenter {
         this.view = view;
     }
 
-    private int calcNewModelValue(int index) {
-        int currentValue = model.getElementValueByKey(index);
-        return currentValue + 1;
+    private void calcNewModelValue(int key) {
+        int newValue = model.getElementValueByKey(key) + 1;
+        model.setElementValueByKey(key, newValue);
+        view.setButtonText(key, newValue);
     }
 
     void onClick(int btnNum) {
-        int newModelValue;
-        switch (btnNum) {
-            case 0:
-                newModelValue = calcNewModelValue(0);
-                model.setElementValueByKey(0, newModelValue);
-                view.setButtonText(0, newModelValue);
-                break;
-            case 1:
-                newModelValue = calcNewModelValue(1);
-                model.setElementValueByKey(1, newModelValue);
-                view.setButtonText(1, newModelValue);
-                break;
-            case 2:
-                newModelValue = calcNewModelValue(2);
-                model.setElementValueByKey(2, newModelValue);
-                view.setButtonText(2, newModelValue);
-                break;
-        }
+        calcNewModelValue(btnNum);
     }
 }
