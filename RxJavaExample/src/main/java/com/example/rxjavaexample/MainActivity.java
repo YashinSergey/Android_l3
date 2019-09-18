f7c938bd9c9524c2d714a6448762c921a3e1dcd0
mr.Blue <yashinsergey125@gmail.com>
2019-09-18 15:53:41 +0300

    TextChangedListener whith TextWatcher added

--- RxJavaExample/src/main/java/com/example/rxjavaexample/MainActivity.java
+++ RxJavaExample/src/main/java/com/example/rxjavaexample/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.rxjavaexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+
+    private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        editText.addTextChangedListener(new Watcher());
+    }
+
+    private void initViews() {
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+    }
+
+    class Watcher implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            textView.setText(s);
+        }
     }
 }
